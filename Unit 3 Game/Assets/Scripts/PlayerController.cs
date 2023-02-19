e7d4ec1d32d7e5872ac2d4e25913a39749862526
Max Field <maxfield7766@gmail.com>
2023-02-19 22:29:19 +1300

    sound effects & particles

--- Unit 3 Game/Assets/Scripts/PlayerController.cs
+++ Unit 3 Game/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,13 @@ public class PlayerController : MonoBehaviour
     Rigidbody playerRb;
     private Animator playerAnimations;
 
+    public ParticleSystem explosion;
+    public ParticleSystem run;
+
+    public AudioClip jump;
+    public AudioClip crash;
+    private AudioSource playerAudio;
+
     public int jumpForce;
     public int gravityModifier;
 
@@ -20,6 +27,8 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody>();
         playerAnimations = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
+
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -30,13 +39,20 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
             playerAnimations.SetTrigger("Jump_trig");
+            playerAudio.PlayOneShot(jump, 1f);
         }
         if (playerLives == 0)
         {
             playerAnimations.SetBool("Death_b", true);
             playerAnimations.SetInteger("DeathType_int", 2);
             gameOver = true;
-            
+            run.Stop();
+        }
+        
+        if (!isGrounded && !gameOver)
+        {
+            run.Play();
+
         }
         
     }
@@ -57,6 +73,8 @@ public class PlayerController : MonoBehaviour
             Destroy(other.gameObject);
             playerLives -= 1;
             Debug.Log("-1 LIFE");
+            explosion.Play();
+            playerAudio.PlayOneShot(crash, 1f);
         }
     }
 
